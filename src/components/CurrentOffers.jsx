0f757588be37d8efc321b237bac9cddd25c59dc1
stanli206 <stantilin@gmail.com>
2025-02-26 23:22:29 +0530

    latestCommit01

--- src/components/CurrentOffers.jsx
+++ src/components/CurrentOffers.jsx
@@ -4,7 +4,7 @@ function CurrentOffers() {
   return (
     <div className="flex pt-25 sm:pt-16 md:pt-18">
       <img
-        src="./src/assets/pc_unrec._CB568620391_.gif"
+        src="/pc_unrec._CB568620391_.gif"
         className="h-40 w-full "
         alt=""
       />
